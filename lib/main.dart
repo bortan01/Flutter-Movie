4923a9960c4ef3a09cde8d47d333a49bd48bdfe2
Boris Ricardo Miranda <brmiranda009@gmail.com>
2022-05-29 11:26:56 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_movie/src/screen/details_screen.dart';
+import 'package:flutter_movie/src/screen/home_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Películas',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => const HomeScreen(),
+        'details': (_) => const DetailsScreen()
+
+      },
+      
+    );
+  }
+}
---
+++ lib/src/screen/details_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+   
+  const DetailsScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("Peliculas en Cines"),
+        elevation: 0,
+      ),
+      body: const Center(
+         child: Text('DetailsScreen'),
+      ),
+    );
+  }
+}
